f2a08f494a6d8f6c0087fb5c62ef65332c04a33e
Vaishnav Bengeri <vbengeri0107@gmail.com>
2021-03-08 13:14:48 -0500

    Update MineSweeper.py
    So me and Shrav can work on this

--- MineSweeper.py
+++ MineSweeper.py
@@ -1,7 +1,7 @@
 import random
 
 class Cell:
-    
+
     def __init__(self, safe, numMinesInden, clue, numSafeNeighbors, numHiddenSquares):
         self.safe = safe
         self.numMinesInden = numMinesInden
@@ -18,17 +18,14 @@ class Cell:
         else:
             return ("Cell not initialized")"""
 
-
-
 def populateMines(minMap, n, d):
     testList = list(range(0, d))
-  
     numMines = n
     while(numMines > 0):
         x = random.choice(testList)
         y = random.choice(testList)
-        print(x)
-        print(y)
+        #print(x)
+        #print(y)
         if (minMap[x][y].returnSafe()):
             minMap[x][y].safe = False
             numMines = numMines - 1
@@ -38,21 +35,21 @@ def printboard(minMap, d):
     for i in range(d):
         for j in range(d):
             if(minMap[i][j].safe == True):
-                print("*\t")
+                print("_\t", end=' ')
             else:
-                print("_\t")
+                print("*\t", end=' ')
         print('\n')
 
 def createBoard(n, d):
     minMap = [[Cell(True, None, None, None, None) for j in range(d)] for i in range(d)]
- 
     minMap = populateMines(minMap, n, d)
     printboard(minMap, d)
 
-
+def makeInferences(minMap, )
+    cluesDictionary = {}
+    
 
 d = 5
 n = 10
 
 createBoard(n, d)
-
