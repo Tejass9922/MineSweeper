fa1b8a47f7f093cf33b323b3f7071eee3bc1c88f
Shravan Patel <shravanpatel1999@gmail.com>
2021-03-08 17:41:09 -0500

    Update MineSweeper.py

--- MineSweeper.py
+++ MineSweeper.py
@@ -1,13 +1,17 @@
 import random
 
 class Cell:
-
-    def __init__(self, safe, numMinesInden, clue, numSafeNeighbors, numHiddenSquares):
+    def __init__(self, safe, numMinesInden, clue, numSafeNeighbors, numHiddenSquares, visited, location):
         self.safe = safe
         self.numMinesInden = numMinesInden
         self.clue = clue
         self.numSafeNeighbors = numSafeNeighbors
         self.numHiddenSquares = numHiddenSquares
+        self.visited = visited
+        self.location = location
+    def __eq__(self, other):
+        return (self.location == other.location)
+
     def returnSafe(self):
         return self.safe
     """def __repr__(self):
@@ -41,15 +45,109 @@ def printboard(minMap, d):
         print('\n')
 
 def createBoard(n, d):
-    minMap = [[Cell(True, None, None, None, None) for j in range(d)] for i in range(d)]
+    minMap = [[Cell(True, None, None, None, None, 0, (j, i)) for j in range(d)] for i in range(d)]
     minMap = populateMines(minMap, n, d)
     printboard(minMap, d)
+    return minMap
 
-def makeInferences(minMap, )
-    cluesDictionary = {}
-    
+row = [-1, -1, -1, 0, 0, 1, 1, 1]
+col = [-1, 0, 1, -1, 1, -1, 0, 1]
+
+
+
+def getNeighborsandClue(board, cell):
+    clue = 0
+    neighbors  = []
+    n = len(board)
+    for i in range(8):
+        x = row[i] + cell.location[0]
+        y = col[i]  + cell.location[1]
+        if x >= 0 or x < n or y >=0 or y < n:
+            neighbors.append(board[x][y])
+            if (board[x][y].safe == False):
+                clue += 1
+            
+
+    return (neighbors, clue)
+
+
+
+
 
 d = 5
 n = 10
 
-createBoard(n, d)
+
+minMap = createBoard(n, d)
+
+
+
+KB = {}
+
+def kbEquation(minMap, cell):
+    (neighbors, clue) = getNeighborsandClue(minMap, cell)
+    cell.clue = clue
+    KB[cell] = neighbors
+    """
+    retrie neighbor locations
+    retrieve clue value
+    create dictionary element
+        key: Object containing location and clue 
+        value: set of neighbors (objects containing locations)"""
+    return 
+
+
+    
+def solver(minMap, d, n):
+    switch = 0 #used for breaking out of loop when you find a 0 (safe) value
+    numMines = n
+    
+    while (numMines > 0):
+        switch = 0
+       
+        while (switch != 1): #while you have not found a new spot to uncover 
+            
+            for keyA in KB:
+                for keyB in KB:
+                    if (not keyA == keyB):
+                        keySetA = KB[keyA] #gets the set of neighbors from the equation
+                        keySetB = KB[keyB] #gets the set of neighbors from the equation
+
+                        if (len(keySetB) >= len(keySetA) and keyB.clue > keyA.clue): #checks to make sure B has a larger sets size and clue value
+                            newSet = keySetB.difference(keySetB) #gets the difference in sets
+                            newClue = (keyB.clue - keyA.clue) #gets the difference in clue values
+
+                        #if clue is 0 
+                        if (newClue == 0):
+                            for i in newSet:
+                                i.safe = True
+                        
+                        #if clue is 1
+                        if ((newClue/len(newSet) == 1):
+                            for i in newSet:
+                                i.safe = False 
+                        #if clue > 1
+
+                        """
+                        check to see if the length of KEYSetB is greater than keySetA and that KeyB clue is greater than KeyA clue
+                        if it is
+                            subtract keySetA from keySetB 
+                                *check length of subtraction to see if there was any difference
+                                create a new set (equation value)
+                            subtract KeyA clue from KeyB clue
+                                create a new clue (index value)
+                            create a new equation
+                            check if the clue == 0 or 1
+                                modify Map to reflect that
+                                if clue == 1
+                                    switch = 1
+                                        ***Implement:
+                                            chooses that value as the next location
+                                            creates a equation
+                                            
+                                else 
+                                    numMines -= 1
+
+                        if not 
+                            contiue over (**Don't want negative numbers)
+                        """
